182c62282db55b0bf032c38ba7de7eefab6b95b4
arch-imp <kentho_98@yahoo.com>
2018-06-11 10:26:55 -0600

    Add PersonController.

---
+++ Part 1 Getting started with Web API and OData V4/WebAPIODataV4/Controllers/PersonController.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Web.Http;
+using System.Web.OData;
+using WebAPIODataV4.Models;
+
+namespace WebAPIODataV4.Controllers
+{
+    public class PersonController : ODataController
+    {
+        readonly DomainModel _db = new DomainModel();
+
+        [EnableQuery(PageSize = 20)]
+        public IHttpActionResult Get()
+        {
+            return Ok(_db.Person.AsQueryable());
+        }
+
+        public IHttpActionResult Get([FromODataUri] int key)
+        {
+            return Ok(_db.Person.SingleOrDefault(t => t.BusinessEntityID == key));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
