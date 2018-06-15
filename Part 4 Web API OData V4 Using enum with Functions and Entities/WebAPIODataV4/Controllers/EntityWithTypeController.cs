4897435e9ebe37d36e9b923f1368a1f7b1b5ab66
arch-imp <kentho_98@yahoo.com>
2018-06-15 13:47:36 -0600

    Add EntityWithTypeController.

---
+++ Part 4 Web API OData V4 Using enum with Functions and Entities/WebAPIODataV4/Controllers/EntityWithTypeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.OData;
+using System.Web.OData.Routing;
+using WebAPIODataV4.Models;
+
+namespace WebAPIODataV4.Controllers
+{
+    [ODataRoutePrefix("EntityWithEnum")]
+    public class EntityWithEnumController : ODataController
+    {
+        private readonly List<EntityWithEnum> someData = new List<EntityWithEnum>();
+
+        public EntityWithEnumController()
+        {
+            someData.Add(new EntityWithEnum { Description = "test1", Name = "Van", PhoneNumberType = PhoneNumberTypeEnum.Home });
+            someData.Add(new EntityWithEnum { Description = "test2", Name = "Bill", PhoneNumberType = PhoneNumberTypeEnum.Work });
+            someData.Add(new EntityWithEnum { Description = "test3", Name = "Rob", PhoneNumberType = PhoneNumberTypeEnum.Cell });
+        }
+
+        [ODataRoute]
+        [EnableQuery(PageSize = 20)]
+        public IHttpActionResult Get()
+        {
+            return Ok(someData);
+        }
+
+        [ODataRoute]
+        [EnableQuery(PageSize = 20)]
+        public IHttpActionResult Get([FromODataUri] string key)
+        {
+            if (someData.Exists(t => t.Name == key))
+            {
+                return Ok(someData.FirstOrDefault(t => t.Name == key));
+            }
+
+            return BadRequest("key does not key");
+        }
+    }
+}
