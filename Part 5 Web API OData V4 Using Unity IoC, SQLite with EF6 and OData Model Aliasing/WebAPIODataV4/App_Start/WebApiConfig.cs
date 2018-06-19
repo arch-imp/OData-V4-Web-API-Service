f7d7b56c427140656a9d48c1b2d60ae953076c5c
arch-imp <kentho_98@yahoo.com>
2018-06-19 10:32:33 -0600

    Import current state from Part 4.

--- Part 5 Web API OData V4 Using Unity IoC, SQLite with EF6 and OData Model Aliasing/WebAPIODataV4/App_Start/WebApiConfig.cs
+++ Part 5 Web API OData V4 Using Unity IoC, SQLite with EF6 and OData Model Aliasing/WebAPIODataV4/App_Start/WebApiConfig.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+using WebAPIODataV4.Models;
 
 namespace WebAPIODataV4
 {
@@ -19,6 +19,53 @@ namespace WebAPIODataV4
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Count().Filter().OrderBy().Expand().Select().MaxTop(null);
+            config.MapODataServiceRoute("odata", "odata", model: GetModel());
+        }
+
+        public static Microsoft.OData.Edm.IEdmModel GetModel()
+        {
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+
+            builder.EntitySet<Address>("Address");
+            builder.EntitySet<AddressType>("AddressType");
+            builder.EntitySet<BusinessEntity>("BusinessEntity");
+            builder.EntitySet<BusinessEntityAddress>("BusinessEntityAddress");
+            builder.EntitySet<BusinessEntityContact>("BusinessEntityContact");
+            builder.EntitySet<ContactType>("ContactType");
+            builder.EntitySet<CountryRegion>("CountryRegion");
+            builder.EntitySet<EmailAddress>("EmailAddress");
+            builder.EntitySet<Password>("Password");
+            builder.EntitySet<Person>("Person");
+            builder.EntitySet<PersonPhone>("PersonPhone");
+            builder.EntitySet<PhoneNumberType>("PhoneNumberType");
+            builder.EntitySet<StateProvince>("StateProvince");
+
+            EntitySetConfiguration<EntityWithEnum> entitesWithEnum
+                = builder.EntitySet<EntityWithEnum>("EntityWithEnum");
+
+            FunctionConfiguration functionEntitesWithEnum
+                = entitesWithEnum.EntityType.Collection.Function("PersonSearchPerPhoneType");
+
+            functionEntitesWithEnum.Parameter<PhoneNumberTypeEnum>("PhoneNumberTypeEnum");
+            functionEntitesWithEnum.ReturnsCollectionFromEntitySet<EntityWithEnum>("EntityWithEnum");
+
+            EntitySetConfiguration<ContactType> contactType = builder.EntitySet<ContactType>("ContactType");
+            var actionY = contactType.EntityType.Action("ChangePersonStatus");
+            actionY.Parameter<string>("Level");
+            actionY.Returns<bool>();
+
+            var changePersonStatusAction = contactType.EntityType.Collection.Action("ChangePersonStatus");
+            changePersonStatusAction.Parameter<string>("Level");
+            changePersonStatusAction.Returns<bool>();
+
+            EntitySetConfiguration<Person> persons = builder.EntitySet<Person>("Person");
+
+            FunctionConfiguration myFirstFunction = persons.EntityType.Collection.Function("MyFirstFunction");
+            myFirstFunction.ReturnsCollectionFromEntitySet<Person>("Person");
+
+            return builder.GetEdmModel();
         }
     }
 }
