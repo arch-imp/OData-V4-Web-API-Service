f0cd4cb5901e8c19e0f5b02861091544d7a2576e
arch-imp <kentho_98@yahoo.com>
2018-06-22 09:58:11 -0600

    Import current state from Part 6.

---
+++ Part 7 Web API OData V4 Using a Singleton/WebAPIODataV4/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.OData.Builder;
+
+namespace WebAPIODataV4.Models
+{
+    public class Player
+    {
+        [Key]
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        [Contained]
+        public virtual PlayerStats PlayerStats { get; set; }
+    }
+}
